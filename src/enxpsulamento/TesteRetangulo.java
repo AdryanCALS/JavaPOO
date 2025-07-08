624ac4d70176d19c9e9f85368a93bd589ad5d0fd
Adryan Lima <acals@poli.br>
2025-07-08 15:45:15 -0300

    Tudo da aula02 POO

---
+++ src/enxpsulamento/TesteRetangulo.java
@@ -0,0 +1,13 @@
+package enxpsulamento;
+
+public class TesteRetangulo {
+
+	public static void main(String[] args) {
+		Retangulo r1 = new Retangulo(10,5);
+		
+		System.out.println(r1.calcularArea());
+		System.out.println(r1.calcularPerimetro());
+
+	}
+
+}
